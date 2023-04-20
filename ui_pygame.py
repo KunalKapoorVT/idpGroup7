1593aa9f66ccf45c295f2e9ec90fe5882ace35ff
KunalKapoorVT <111710078+KunalKapoorVT@users.noreply.github.com>
2023-04-20 02:39:44 -0400

    tractor Report gui

--- ui_pygame.py
+++ ui_pygame.py
@@ -1,11 +1,17 @@
-from bleak import BleakScanner, BleakClient;
+from bleak import BleakScanner, BleakClient
 import sys, asyncio
 import pygame
 import Tractor
 
 
+def drawTrace(surface, x, y, prevX, prevY):
+    rec = pygame.Rect(surface, x - prevX, y - prevY)
+
+    pygame.draw.rect(surface, (0, 0, 0), rec)
+
+
 class Button():
-    def __init__(self, surface=None, x=None, y=None, width=50, height=50, color = (128,128,128)):
+    def __init__(self, surface=None, x=None, y=None, width=50, height=50, color=(128, 128, 128)):
         self.surface = surface
         self.x = x
         self.y = y
@@ -16,46 +22,45 @@ class Button():
 
     def setText(self, text):
 
-        self.text = font.render(text, True, (0,0,0))
-        
+        self.text = font.render(text, True, (0, 0, 0))
+
     def draw(self, surface=None, x=None, y=None):
-        if(surface == None):
+        if (surface == None):
             surface = self.surface
-        if(x == None):
+        if (x == None):
             x = self.x
-        if(y == None):
+        if (y == None):
             y = self.y
 
-        pygame.draw.rect(surface, self.color, (x,y,self.width,self.height))
-        surface.blit(self.text, (x+50,y+40,150,100))
+        pygame.draw.rect(surface, self.color, (x, y, self.width, self.height))
+        surface.blit(self.text, (x + 50, y + 40, 150, 100))
 
     def bind(self, function):
-        
+
         self.run = function
 
     def inside(self, x, y):
-        return x > self.x  and  x < self.x+self.width and y > self.y and y < self.y+self.height
+        return x > self.x and x < self.x + self.width and y > self.y and y < self.y + self.height
 
 
 async def main():
-    tractor = Tractor.Tractor()
+    t = Tractor.Tractor()
 
-    await tractor.connect()
+    await t.connect()
 
-    await tractor.setRecv()
+    await t.setRecv()
 
     pygame.init()
-    screen = pygame.display.set_mode((600,600))
+    screen = pygame.display.set_mode((600, 600))
 
-    pygame.display.set_caption("IDP Group 7 Pygame Tractor GUI")
+    pygame.display.set_caption("IDP Group 7 Pygame Tractor GUI - Main Menu")
 
     global font
     font = pygame.font.Font('freesansbold.ttf', 32)
 
-    startButton = Button(screen, 50,50,500,100)
-    stopButton = Button(screen, 50,200,500,100)
-    reportButton = Button(screen, 50,350,500,100)
-
+    startButton = Button(screen, 50, 50, 500, 100)
+    stopButton = Button(screen, 50, 200, 500, 100)
+    reportButton = Button(screen, 50, 350, 500, 100)
 
     startButton.bind(lambda: t.send(b'Go'))
     stopButton.bind(lambda: t.send(b'Stop'))
@@ -64,39 +69,51 @@ async def main():
     stopButton.setText("Stop Tractor")
     reportButton.setText("Generate Report")
 
+    tractorImage = pygame.image.load('tractorJPEG.jpg').convert()
+    tractorImage = pygame.transform.scale(tractorImage, (25, 25))
+
     running = True
+    reportMenu = False
+    pygame.display.flip() #run first frame because of inconsistent bug
     while running:
+        screen.fill((255, 255, 255))
 
-        screen.fill((255,255,255))
+        if not reportMenu:
+            startButton.draw()
+            stopButton.draw()
+            reportButton.draw()
 
-        startButton.draw()
-        stopButton.draw()
-        reportButton.draw()
-
-        pygame.display.update()
-        
         for event in pygame.event.get():
-            
             if event.type == pygame.QUIT:
-                running = False;
-                await tractor.quit()
+                running = False
                 pygame.quit()
-                
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP and not reportMenu:
                 pos = pygame.mouse.get_pos()
 
                 if startButton.inside(pos[0], pos[1]):
                     await startButton.run()
-                    
+
                 if stopButton.inside(pos[0], pos[1]):
                     await stopButton.run()
-                    
+
                 if reportButton.inside(pos[0], pos[1]):
                     await reportButton.run()
+                    reportMenu = True
+                    pygame.display.set_caption("IDP Group 7 Pygame Tractor GUI - Trip Report")
 
+            if reportMenu:
+                screen.blit(tractorImage, (t.PosX, t.PosY))
+                if t.AngleZ != t.idealAngleZ:
+                    tractorImage = pygame.transform.rotate(tractorImage, t.AngleZ)
 
+                #draw behind if changed pos
+                if t.PosX != t.prevPosX:
+                    drawTrace(screen, t.PosX, t.PosY, t.prevPosX, t.prevPosY)
+
+        pygame.display.update()
 
         await asyncio.sleep(0.1)
 
+
 asyncio.run(main())
-    
+
