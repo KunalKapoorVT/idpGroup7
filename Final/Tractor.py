38dbcd2868fd82af51f0fb65814df6f6c15f9e4b
KunalKapoorVT <111710078+KunalKapoorVT@users.noreply.github.com>
2023-04-24 02:10:17 -0400

    Final Folder Upload
    NOTE: Not tested just yet

---
+++ Final/Tractor.py
@@ -0,0 +1,159 @@
+from bleak import BleakClient, BleakScanner
+from math import sin, cos
+import asyncio
+import time
+
+
+async def scan():
+    devices = await BleakScanner.discover()
+    return devices
+
+
+async def connEst(tractorCli):
+    conn = await tractorCli.connect()
+    return conn
+
+
+class Tractor():
+    def __init__(self):
+        self.PosX = 0
+        self.PosY = 0
+        self.AngleZ = 0
+        self.VelX = 0
+        self.velY = 0
+
+        self.estimatedXVals = []
+        self.estimatedYVals = []
+
+        self.prevEstX = 0
+        self.prevEstY = 0
+
+        self.prevPosX = 0
+        self.prevPosY = 0
+
+        self.moving = False
+        self.obstacle = False
+
+        self.idealAngleZ = 0
+
+        self.tapes = []
+        self.estTapes = []
+        self.obstacles = []
+        self.estObstacles = []
+
+        self.userConrolEvents = []
+
+        # permanent mac address of the tractor
+        self.mac_addr = "94:A9:A8:3A:4D:8B"
+        self.gatt_char = "0000FFE1-0000-1000-8000-00805f9b34fb"
+
+        self.bluetoothClient = None
+        self.startTime = time.time()
+
+        self.recvLog = ''
+
+    def time(self):
+
+        return time.time() - self.startTime
+
+    async def quit(self):
+
+        await self.bluetoothClient.stop_notify(self.gatt_char)
+
+        await self.bluetoothClient.disconnect()
+
+    async def send(self, msg):
+
+        print(msg)
+
+        await self.bluetoothClient.write_gatt_char(self.gatt_char, msg)
+
+    def handleMsg(self, msg):
+
+        print(msg)
+
+        if msg == "Go Recieved":
+            self.moving = True
+            self.userConrolEvents.append((self.time(), self.moving))
+
+        if msg == "Stop Recieved":
+            self.moving = False
+            self.userConrolEvents.append((self.time(), self.moving))
+
+        if msg == "Obstacle":
+            self.moving = False
+            self.obstacle = True
+            self.obstacles.append((self.time(), self.PosX, self.PosY))
+            self.estObstacles.append((self.time(), self.prevEstX, self.prevEstY))
+
+        if msg == "Obstacle Cleared":
+            self.moving = True
+            self.obstacle = False
+            prevObs = self.obstacles[-1]
+            prevEstObs = self.estObstacles[-1]
+            self.obstacles[-1] = (prevObs[0], self.time(), prevObs[1], prevObs[2])
+            self.estObstacles[-1] = (prevEstObs[0], self.time(), prevEstObs[1], prevEstObs[2])
+
+        if msg == "Read Tape":
+            self.tapes.append((self.time(), self.PosX, self.PosY))
+            self.estTapes.append((self.time(), self.prevEstX, self.prevEstY))
+
+        if msg.startswith("MOVING="):
+            self.moving = bool(int(msg[len("MOVING="):]))
+            self.userConrolEvents.append((self.time(), self.moving))
+
+        if msg.startswith("GOALANGLE="):
+            self.idealAngleZ = float(msg[len("GoALANGLE="):])
+
+        if msg.startswith("ANGLEZ="):
+            self.AngleZ = float(msg[len("ANGLEZ="):])
+
+            if (self.moving):
+                self.estimatedXVals.append((self.time(), self.prevEstX - sin(self.AngleZ)))
+                self.estimatedYVals.append((self.time(), self.prevEstY + cos(self.AngleZ)))
+
+                self.prevEstX = self.estimatedXVals[-1][1]
+                self.prevEstY = self.estimatedYVals[-1][1]
+
+        if msg.startswith("PosX="):
+            self.prevPosX = self.PosX
+            self.PosX = float(msg[len("PosX="):])
+
+        if msg.startswith("PosY="):
+            self.prevPosY = self.PosY
+            self.PosY = float(msg[len("PosY="):])
+
+        if msg.startswith("VelX="):
+            self.VelX = float(msg[len("VelX="):])
+
+        if msg.startswith("VelY="):
+            self.VelY = float(msg[len("VelY="):])
+
+    def processRecvLog(self):
+
+        if '\r\n' in self.recvLog:
+            msg = self.recvLog[:self.recvLog.index('\r\n')]
+            self.recvLog = self.recvLog[self.recvLog.index('\r\n') + 2:]
+            self.handleMsg(msg)
+
+    def receiveNotify(self, handle, data):
+
+        print(data)
+
+        self.recvLog += data.decode()
+
+        self.processRecvLog();
+
+    async def connect(self):
+
+        self.bluetoothClient = BleakClient(self.mac_addr);
+
+        await self.bluetoothClient.connect()
+
+    async def setRecv(self):
+
+        await self.bluetoothClient.start_notify(self.gatt_char, self.receiveNotify)
+
+    async def disconnect(self):
+
+        await self.bluetoothClient.disconnect()
---
+++ Final/finalArduino/finalArduino.ino
@@ -0,0 +1,338 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+const double pi = 3.14159265358979323846264;
+
+//Used for gyro
+const double minAngleFactor = 10000;
+
+//motor pins
+const int motorLForward = 11;
+const int motorLBackward = 10;
+const int motorRForward = 5;
+const int motorRBackward = 6;
+
+//push button
+const int buttonIn = A3;
+
+//Bluetooth
+const int BT_PIN_RXD = 7;
+const int BT_PIN_TXD = 8;
+
+//IR photodiode
+const int IR_PIN = A0;
+int val = 0;
+int limit = 1010;
+bool turning = false;
+int turnDelay = 0;
+int lastUpdateMS = 0;
+
+//gyro
+unsigned long gyroTimer = 0;
+double posX = 0.0;
+double posY = 0.0;
+double velX = 0.0;
+double velY = 0.0;
+double gyroTotalAccX;
+double gyroTotalAccY;
+unsigned long sendDataTimer = 0;
+
+double lineAngleDeg = 0;
+double lineX = 0;
+double tractorAngle = 0;
+double tractorX = 0;
+
+//push button
+int lowCount = 0;
+int highCount = 0;
+
+//states
+bool buttonState = false;
+bool buttonControl = false;
+bool moving = false;
+String turn = "Forward";
+int counter = 0;
+
+bool obstacle = false;
+
+SoftwareSerial bluetooth(BT_PIN_RXD, BT_PIN_TXD);
+
+MPU6050 gyroscope(Wire);
+
+void setup() {
+  pinMode(motorLForward, OUTPUT);
+  pinMode(motorLBackward, OUTPUT);
+  pinMode(motorRForward, OUTPUT);
+  pinMode(motorRBackward, OUTPUT);
+
+  //pinMode(trigPin, OUTPUT);
+  //pinMode(echoPin, INPUT);
+
+  //  Serial.begin(9600);
+  Wire.begin();
+  bluetooth.begin(9600);
+
+  byte gyroStatus = gyroscope.begin();
+  bluetooth.print(F("MPU6050 status: "));
+  bluetooth.println(gyroStatus);
+
+  bluetooth.println(F("Calculating offsets, do not move MPU6050"));
+  delay(100);
+  gyroscope.calcOffsets(true, true);  // gyro and accelero
+  bluetooth.println("Done!\n");
+}
+
+void loop() {
+
+  gyroscope.update();
+
+  updateGyroPos();
+
+  sendLocationData();
+
+  ultraEStop();
+
+  IRtest();
+
+  if (turning)
+    turnTimer();
+
+  if (bluetooth.available()) {
+    String btString = bluetooth.readString();
+    if (btString == "Go") {
+      moving = true;
+      bluetooth.println("Go Recieved");
+    } else if (btString == "Stop") {
+      moving = false;
+      bluetooth.println("Stop Recieved");
+    } else if (btString == "Right") {
+      lineAngleDeg -= 90;
+      bluetooth.println("R");
+      bluetooth.println(lineAngleDeg);
+    } else if (btString == "Left") {
+      lineAngleDeg += 90;
+      bluetooth.println("L");
+      bluetooth.println(lineAngleDeg);
+    }
+  }
+
+  bool buttonVal = analogRead(buttonIn) > 512;
+  //bluetooth.println(analogRead(buttonIn));
+  updateButton(buttonVal);
+
+  //bluetooth.println(moving);
+  if (moving) {
+    driveForward(false);
+  } else {
+    stopTractor();
+  }
+}
+
+void stopTractor() {
+  digitalWrite(motorLForward, LOW);
+  digitalWrite(motorLBackward, LOW);
+  digitalWrite(motorRForward, LOW);
+  digitalWrite(motorRBackward, LOW);
+}
+
+void sendLocationData(){
+  
+  if (millis() - sendDataTimer > 5000) {
+    
+    bluetooth.print("ANGLEZ=");
+    bluetooth.println(gyroscope.getAngleZ());
+
+    bluetooth.print("PosX=");
+    bluetooth.println(posX);
+    bluetooth.print("PosY=");
+    bluetooth.println(posY);
+    bluetooth.print("VelX=");
+    bluetooth.println(velX);
+    bluetooth.print("VelY=");
+    bluetooth.println(velY);
+
+    sendDataTimer = millis();
+  }
+  
+}
+
+void driveForward(bool considerHorizontal) {
+
+  double idealAngleDeg = lineAngleDeg;
+
+  if (considerHorizontal) {
+
+    //the distance from the track, xDist, how far the tractor is to the right of its straight line
+    double xDist = tractorX - lineX;
+
+    //minAngleDeg: the ideal angle to hold, from relative to the angle of the line
+    //when the tractor is on the right, this is a positive (ccw) angle
+    //when it is on the left, xDist is negative, so the angle is clockwise
+    idealAngleDeg = xDist / minAngleFactor + lineAngleDeg;
+  }
+
+  //the furthest you should go, relative to the line, is perpendicular
+  int maxAngleDeg = 90 + lineAngleDeg;
+  int minAngleDeg = -90 + lineAngleDeg;
+
+  //bound the ideal angle between the max and min
+  idealAngleDeg = max(min(idealAngleDeg, maxAngleDeg), minAngleDeg);
+
+  //take the ideal angle and the current angle and create relative motor speeds
+  double angleDiff = tractorAngle - idealAngleDeg;
+
+
+  int motorLSpeed = 0;  // -256 to 255
+  int motorRSpeed = 0;  // -256 to 255
+
+  //at angleDiff>=90: L = 255, R = -255
+  //at angleDiff=0: L = 255, R = 255
+  //at angleDiff<=-90: L = -255, R = 255
+
+
+  motorLSpeed = 255 * (1 - angleDiff / 45);
+  motorLSpeed = max(min(motorLSpeed, 255), -255);
+
+
+  motorRSpeed = 255 * (1 + angleDiff / 45);
+  motorRSpeed = max(min(motorRSpeed, 255), -255);
+
+  //    Serial.print("Motor L Speed: ");Serial.println(motorLSpeed);
+  //    Serial.print("Motor R Speed: ");Serial.println(motorRSpeed);
+
+  driveTractor(motorLSpeed, motorRSpeed);
+}
+
+void updateGyroPos() {
+  int dt = millis() - gyroTimer;
+  double Zangle = gyroscope.getAngleZ();
+  tractorAngle = Zangle;
+  velX += (gyroscope.getAccX() * cos(Zangle * 2 * pi / 360) + gyroscope.getAccY() * sin(Zangle * 2 * pi / 360)) * dt;
+  velY += (gyroscope.getAccY() * cos(Zangle * 2 * pi / 360) - gyroscope.getAccX() * sin(Zangle * 2 * pi / 360)) * dt;
+  posX += velX * dt;
+  posY += velY * dt;
+  gyroTimer = millis();
+}
+
+void driveTractor(int LSpeed, int RSpeed) {
+
+  //convert wheel speed into forward and backward signal
+  byte motorLForwardSpeed = (LSpeed > 0) ? LSpeed : 0;
+  byte motorRForwardSpeed = (RSpeed > 0) ? RSpeed : 0;
+  byte motorLBackwardSpeed = (LSpeed < 0) ? -LSpeed : 0;
+  byte motorRBackwardSpeed = (RSpeed < 0) ? -RSpeed : 0;
+
+  //set the wheels' speeds
+  analogWrite(motorLForward, motorLForwardSpeed);
+  analogWrite(motorLBackward, motorLBackwardSpeed);
+  analogWrite(motorRForward, motorRForwardSpeed);
+  analogWrite(motorRBackward, motorRBackwardSpeed);
+}
+
+void onButtonStateHigh() {
+  bluetooth.println("Button up");
+  buttonControl = !buttonControl;
+  moving = buttonControl;
+  bluetooth.print("MOVING=");
+  bluetooth.println(moving);
+  /*if (buttonControl)
+    {
+    //bluetooth.println(("Restarting"));
+    bluetooth.println(F("Calculating offsets, do not move MPU6050"));
+    delay(100);
+    gyroscope.calcOffsets(true,true); // gyro and accelero
+    bluetooth.println("Done!\n");
+    }*/
+}
+
+void onButtonStateLow() {
+  bluetooth.println("Button down");
+  moving = buttonControl;
+}
+
+void updateButton(bool buttonVal) {
+  if (buttonVal) {
+    highCount++;
+    lowCount = 0;
+    if (highCount > 5 && !buttonState) {
+      buttonState = true;
+      onButtonStateHigh();
+    }
+  }
+  if (!buttonVal) {
+    lowCount++;
+    highCount = 0;
+    if (lowCount > 5 && buttonState) {
+      buttonState = false;
+      onButtonStateLow();
+    }
+  }
+}
+
+void IRtest() {
+  val = analogRead(IR_PIN);  //photodiode reading
+  
+  if (val <= limit && turnDelay < 0)  // NO OBSTICLE
+  {
+    //bluetooth.println("Read Floor");
+    //delay(20);
+    turning = false;
+  } else if (val > limit && !turning)  // OBSTICLE DETECTED
+  {
+    bluetooth.println("Read Tape");
+    //delay(20);
+    //makeNextTurn();
+    //turnDelay = 200 for left 300 for right
+    if (counter <= 1) {
+      turnDelay = 200;
+    } else if (counter <= 3) {
+      turnDelay = 300;
+    } else if (counter <= 5) {
+      turnDelay = 200;
+    }
+
+    lastUpdateMS = millis();
+    turning = true;
+  }
+}
+
+void makeNextTurn() {
+  if (counter <= 1) {
+    lineAngleDeg += 90;
+  } else if (counter <= 3) {
+    lineAngleDeg -= 90;
+    bluetooth.println("R");
+  } else if (counter <= 5) {
+    lineAngleDeg += 90;
+    bluetooth.println("L");
+  }
+  bluetooth.print("GOALANGLE=");
+  bluetooth.println(lineAngleDeg);
+  counter++;
+}
+
+void ultraEStop() {
+  if (digitalRead(3) == 0)  {
+    if (moving) {
+      moving = false;
+      obstacle = true;
+      bluetooth.println("Obstacle Found");
+    }
+  }
+  else if (obstacle)
+  {
+    moving = true;
+    obstacle = false;
+      bluetooth.println("Obstacle Cleared");
+  }
+}
+
+void turnTimer() {
+  if (turnDelay > 0) {
+    turnDelay -= millis() - lastUpdateMS;
+    lastUpdateMS = millis();
+    if (turnDelay <= 0)
+      makeNextTurn();
+  }
+}
