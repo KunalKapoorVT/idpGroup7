574c5941914027dbe36aaee3589592b1fb7f56a6
KunalKapoorVT <111710078+KunalKapoorVT@users.noreply.github.com>
2023-04-26 21:57:22 -0400

    Post validation

--- Final/Tractor.py
+++ Final/Tractor.py
@@ -4,16 +4,6 @@ import asyncio
 import time
 
 
-async def scan():
-    devices = await BleakScanner.discover()
-    return devices
-
-
-async def connEst(tractorCli):
-    conn = await tractorCli.connect()
-    return conn
-
-
 class Tractor():
     def __init__(self):
         self.PosX = 0
@@ -41,7 +31,7 @@ class Tractor():
         self.obstacles = []
         self.estObstacles = []
 
-        self.userConrolEvents = []
+        self.userControlEvents = []
 
         # permanent mac address of the tractor
         self.mac_addr = "94:A9:A8:3A:4D:8B"
@@ -64,46 +54,51 @@ class Tractor():
 
     async def send(self, msg):
 
-        print(msg)
+        print("Sent %s" % (msg.decode()))
 
         await self.bluetoothClient.write_gatt_char(self.gatt_char, msg)
 
     def handleMsg(self, msg):
 
-        print(msg)
-
         if msg == "Go Recieved":
             self.moving = True
-            self.userConrolEvents.append((self.time(), self.moving))
+            self.userControlEvents.append((self.time(), self.moving))
+            print(msg)
 
         if msg == "Stop Recieved":
             self.moving = False
-            self.userConrolEvents.append((self.time(), self.moving))
+            self.userControlEvents.append((self.time(), self.moving))
+            print(msg)
 
-        if msg == "Obstacle":
+        if msg == "Obstacle Found":
             self.moving = False
             self.obstacle = True
-            self.obstacles.append((self.time(), self.PosX, self.PosY))
-            self.estObstacles.append((self.time(), self.prevEstX, self.prevEstY))
+            self.obstacles.append((self.time(), self.PosX, self.PosY, None))
+            self.estObstacles.append((self.time(), self.prevEstX, self.prevEstY, None))
+            print(msg)
 
         if msg == "Obstacle Cleared":
             self.moving = True
             self.obstacle = False
             prevObs = self.obstacles[-1]
             prevEstObs = self.estObstacles[-1]
-            self.obstacles[-1] = (prevObs[0], self.time(), prevObs[1], prevObs[2])
-            self.estObstacles[-1] = (prevEstObs[0], self.time(), prevEstObs[1], prevEstObs[2])
+            self.obstacles[-1] = (prevObs[0], prevObs[1], prevObs[2], self.time())
+            self.estObstacles[-1] = (prevEstObs[0], prevEstObs[1], prevEstObs[2], self.time())
+            print(msg)
 
         if msg == "Read Tape":
-            self.tapes.append((self.time(), self.PosX, self.PosY))
-            self.estTapes.append((self.time(), self.prevEstX, self.prevEstY))
+            self.tapes.append((self.time(), self.PosX, self.PosY, self.AngleZ))
+            self.estTapes.append((self.time(), self.prevEstX, self.prevEstY, self.AngleZ))
+            print(msg)
 
         if msg.startswith("MOVING="):
             self.moving = bool(int(msg[len("MOVING="):]))
-            self.userConrolEvents.append((self.time(), self.moving))
+            self.userControlEvents.append((self.time(), self.moving))
+            print("Started" * self.moving + "Stopped" * (not self.moving))
 
         if msg.startswith("GOALANGLE="):
             self.idealAngleZ = float(msg[len("GoALANGLE="):])
+            print(msg)
 
         if msg.startswith("ANGLEZ="):
             self.AngleZ = float(msg[len("ANGLEZ="):])
@@ -117,11 +112,20 @@ class Tractor():
 
         if msg.startswith("PosX="):
             self.prevPosX = self.PosX
-            self.PosX = float(msg[len("PosX="):])
+            try:
+                self.PosX = float(msg[len("PosX="):])
+            except ValueError:
+                pass
+        ##
+        ##            if msg.startswith("IR="):
+        ##                print(msg[len("IR="):])
 
         if msg.startswith("PosY="):
             self.prevPosY = self.PosY
-            self.PosY = float(msg[len("PosY="):])
+            try:
+                self.PosY = float(msg[len("PosY="):])
+            except ValueError:
+                pass
 
         if msg.startswith("VelX="):
             self.VelX = float(msg[len("VelX="):])
@@ -131,14 +135,12 @@ class Tractor():
 
     def processRecvLog(self):
 
-        if '\r\n' in self.recvLog:
+        while '\r\n' in self.recvLog:
             msg = self.recvLog[:self.recvLog.index('\r\n')]
             self.recvLog = self.recvLog[self.recvLog.index('\r\n') + 2:]
             self.handleMsg(msg)
 
     def receiveNotify(self, handle, data):
-
-        print(data)
 
         self.recvLog += data.decode()
 
